cbd8173f3d5fb3c8c2ff1e8ab8baba2ad5e2cd44
vladimir <vladimir197717@mail.ru>
2023-05-22 13:46:49 +0400

    add annotation @Profiles

--- src/main/java/ru/learnUp/LearnUp20_Spring_Boot/service/processors/SumProcessor.java
+++ src/main/java/ru/learnUp/LearnUp20_Spring_Boot/service/processors/SumProcessor.java
@@ -1,12 +1,16 @@
 package ru.learnUp.LearnUp20_Spring_Boot.service.processors;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import ru.learnUp.LearnUp20_Spring_Boot.service.Operation;
 import ru.learnUp.LearnUp20_Spring_Boot.service.Processor;
 
 @Component
-@Primary
+//@Primary
+@Scope(value = "singleton")
+@Profile("testing")
 public class SumProcessor implements Processor {
 
     @Override
